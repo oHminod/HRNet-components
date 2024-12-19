f36fa8460261cad64af5697d87178effe1a8f383
oHminod <yvesm13@gmail.com>
2024-12-19 18:24:52 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import DatePicker from "../dist/datePicker/DatePicker";
+
+const App = () => (
+  <div>
+    <h1 className="text-4xl font-bold p-6">Hello React</h1>
+    <DatePicker name="dateOfBirth" />
+  </div>
+);
+
+export default App;
