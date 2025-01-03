84986aa85e93819592da4705d8fc024d01b63675
oHminod <yvesm13@gmail.com>
2025-01-03 18:31:50 +0100

    Revert "Adding JSX components without tailwind for better compatibility"
    This reverts commit b27f8949aeadbc96490861ce26349819eb658db5.

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,15 +1,5 @@
 import DatePicker from "./datePicker/DatePicker";
-import DatePickerJS from "./datePickerJS/DatePickerJS";
 import CustomSelect from "./select/Select";
-import CustomSelectJS from "./selectJS/SelectJS";
 import Modal from "./modal/Modal";
-import ModalJS from "./modalJS/ModalJS";
 
-export {
-  DatePicker,
-  DatePickerJS,
-  CustomSelect,
-  CustomSelectJS,
-  Modal,
-  ModalJS,
-};
+export { DatePicker, CustomSelect, Modal };
