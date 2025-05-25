6de01201fbbc705d64852e4d88ae0b1c7e1ab591
oHminod <yvesm13@gmail.com>
2025-05-25 18:33:23 +0200

    only datepicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { CustomSelect, DatePicker } from "./lib";
+import { DatePicker } from "./lib";
 
 const App = () => (
   <div className="p-10">
     <h1 className="text-4xl font-bold p-6">Hello React</h1>
     <DatePicker name="dateOfBirth" />
-    <label htmlFor="customSelect">
-      Animal{"  "}
-      <CustomSelect
-        options={["Veaux", "Vaches", "Cochons"]}
-        placeholder="Select an option"
-        name="customSelect"
-      />
-    </label>
   </div>
 );
 
